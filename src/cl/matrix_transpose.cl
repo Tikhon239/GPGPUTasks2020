b98dd562f12cd079b4c8484873e77a9503ee67fe
Тихон Воробьев <tihonvorobev@Ikhon.local>
2020-10-11 20:02:18 +0300

    task 4.1 done

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,61 @@
-__kernel void matrix_transpose(...)
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#ifndef WORK_GROUP_SIZE
+#define WORK_GROUP_SIZE 16
+#endif
+
+__kernel void simple_matrix_transpose(__global const float* matrix, __global float* matrix_transpose, const unsigned int M, const unsigned int K)
 {
-    // TODO
+    const unsigned int i = get_global_id(0);
+    const unsigned int j = get_global_id(1);
+    if (i > M || j > K)
+        return;
+
+    matrix_transpose[i * M + j] = matrix[j * K + i];
+}
+
+__kernel void bank_conflict_matrix_transpose(__global const float* matrix, __global float* matrix_transpose, const unsigned int M, const unsigned int K)
+{
+    __local float local_matrix[WORK_GROUP_SIZE][WORK_GROUP_SIZE];
+
+    const unsigned int i = get_global_id(0);
+    const unsigned int j = get_global_id(1);
+    if (i >= K || j >= M)
+        return;
+
+    const unsigned int local_i = get_local_id(0);
+    const unsigned int local_j = get_local_id(1);
+    const unsigned int group_i = get_group_id(0);
+    const unsigned int group_j = get_group_id(1);
+
+    local_matrix[local_j][local_i] = matrix[j * K + i];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    matrix_transpose[(group_i * WORK_GROUP_SIZE + local_j) * M + group_j * WORK_GROUP_SIZE + local_i] = local_matrix[local_i][local_j];
+}
+
+__kernel void with_out_bank_conflict_matrix_transpose(__global const float* matrix, __global float* matrix_transpose, const unsigned int M, const unsigned int K)
+{
+    __local float local_matrix[WORK_GROUP_SIZE][WORK_GROUP_SIZE+1];
+
+    const unsigned int i = get_global_id(0);
+    const unsigned int j = get_global_id(1);
+    if (i >= K || j >= M)
+        return;
+
+    const unsigned int local_i = get_local_id(0);
+    const unsigned int local_j = get_local_id(1);
+    const unsigned int group_i = get_group_id(0);
+    const unsigned int group_j = get_group_id(1);
+
+    local_matrix[local_j][local_i] = matrix[j * K + i];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    matrix_transpose[(group_i * WORK_GROUP_SIZE + locala_j) * M + group_j * WORK_GROUP_SIZE + local_i] = local_matrix[local_i][local_j];
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -32,31 +32,29 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
+    unsigned int work_group_size_x = 16;
+    unsigned int global_work_size_x = (K + work_group_size_x - 1) / work_group_size_x * work_group_size_x;
+    unsigned int work_group_size_y = work_group_size_x;
+    unsigned int global_work_size_y = (M + work_group_size_y - 1) / work_group_size_y * work_group_size_y;
+
+
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
-
     as_gpu.writeN(as.data(), M*K);
 
-    ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
-    matrix_transpose_kernel.compile();
-
     {
+        ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "simple_matrix_transpose");
+        matrix_transpose_kernel.compile();
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
-
+            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size_x, work_group_size_y, global_work_size_x,  global_work_size_y), as_gpu, as_t_gpu, M, K);
             t.nextLap();
         }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
+        as_t_gpu.readN(as_t.data(), M*K);
+        std::cout << "simple matrix transpose GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "simple matrix transpose GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
     }
-
-    as_t_gpu.readN(as_t.data(), M*K);
 
     // Проверяем корректность результатов
     for (int j = 0; j < M; ++j) {
@@ -69,7 +67,63 @@ int main(int argc, char **argv)
             }
         }
     }
-    */
+
+    std::cout << "Same matrix!" << std::endl;
+
+    {
+        ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "bank_conflict_matrix_transpose");
+        matrix_transpose_kernel.compile();
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size_x, work_group_size_y, global_work_size_x,  global_work_size_y), as_gpu, as_t_gpu, M, K);
+            t.nextLap();
+        }
+        as_t_gpu.readN(as_t.data(), M*K);
+        std::cout << "bank conflict matrix transpose GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "bank conflict matrix transpose GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
+    }
+
+    // Проверяем корректность результатов
+    for (int j = 0; j < M; ++j) {
+        for (int i = 0; i < K; ++i) {
+            float a = as[j * K + i];
+            float b = as_t[i * M + j];
+            if (a != b) {
+
+                std::cerr << "Not the same! " << j << " " << i << std::endl;
+                return 1;
+            }
+        }
+    }
+
+    std::cout << "Same matrix!" << std::endl;
+
+    {
+        ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "with_out_bank_conflict_matrix_transpose");
+        matrix_transpose_kernel.compile();
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size_x, work_group_size_y, global_work_size_x,  global_work_size_y), as_gpu, as_t_gpu, M, K);
+            t.nextLap();
+        }
+        as_t_gpu.readN(as_t.data(), M*K);
+        std::cout << "with out bank conflict matrix transpose GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "with out bank conflict matrix transpose GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
+    }
+
+    // Проверяем корректность результатов
+    for (int j = 0; j < M; ++j) {
+        for (int i = 0; i < K; ++i) {
+            float a = as[j * K + i];
+            float b = as_t[i * M + j];
+            if (a != b) {
+                std::cerr << "Not the same!" << std::endl;
+                return 1;
+            }
+        }
+    }
+
+    std::cout << "Same matrix!" << std::endl;
 
     return 0;
 }
