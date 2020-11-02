5dd49d1d4eb5bb0bd2f2be274519e7e11b4378b6
Тихон Воробьев <tihonvorobev@Ikhon.local>
2020-11-03 00:20:59 +0300

    try radix sort

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,4 +1,67 @@
-__kernel void radix(__global unsigned int* as)
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#ifndef WORK_GROUP_SIZE
+#define WORK_GROUP_SIZE 4 //128
+#endif
+
+//как довести до рабочего состония
+__kernel void smth(__global unsigned int* as, __global unsigned int* prefix_sum, const unsigned int n)
 {
-    // TODO
+    __local unsigned int local_array[WORK_GROUP_SIZE];
+
+    const unsigned int global_id = get_global_id(0);
+    const unsigned int local_id = get_local_id(0);
+
+    if (global_id < n)
+        local_array[local_id] = as[global_id];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for(int pow = 0; pow <= 2; ++pow) {
+        if (((local_id + 1) >> pow) & 1) {
+            prefix_sum[global_id] += local_array[(local_id + 1) / (1 << pow) - 1];
+        }
+        if (local_id % 2 == 0)
+            local_array[local_id / 2] = local_array[local_id] + local_array[local_id + 1];
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
 }
+
+__kernel void get_inverse_bit(__global unsigned int* as, __global unsigned int* bit_array, const unsigned int n, const unsigned int bit) {
+    const unsigned int global_id = get_global_id(0);
+    if (global_id < n)
+        bit_array[global_id] = 1 - (as[global_id] >> bit) & 1;
+}
+
+__kernel void prefix_sum(__global unsigned int* partial_sum, __global unsigned int* prefix_sum, const unsigned int n, const unsigned int pow) {
+    const unsigned int global_id = get_global_id(0);
+    if (global_id < n) {
+        if (((global_id + 1) >> pow) & 1)
+            prefix_sum[global_id] += partial_sum[(global_id + 1) / (1 << pow) - 1];
+    }
+}
+
+__kernel void partial_sum(__global unsigned int* cur_partial_sum, __global unsigned int* next_partial_sum, const unsigned int n) {
+    const unsigned int global_id = get_global_id(0);
+    if (global_id < n)
+        if (global_id % 2 == 0)
+            next_partial_sum[global_id/2] = cur_partial_sum[global_id] + cur_partial_sum[global_id + 1];
+}
+
+__kernel void radix(__global unsigned int* cur_as, __global unsigned int* next_as, __global unsigned int* prefix_sum, const unsigned int n, const unsigned int bit) {
+    const unsigned int global_id = get_global_id(0);
+    if (global_id < n) {
+        if (1 - (cur_as[global_id] >> bit) & 1)
+            next_as[prefix_sum[global_id] - 1] = cur_as[global_id];
+        else
+            next_as[prefix_sum[n - 1] + global_id - prefix_sum[global_id]] = cur_as[global_id];
+    }
+}
+
+
+
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    unsigned int n = 1024;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -52,36 +52,64 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
-    gpu::gpu_mem_32u as_gpu;
-    as_gpu.resizeN(n);
+
+    std::vector<unsigned int> prefix_cpu = as;
+    for (unsigned int i = 1; i < n; ++i) {
+        prefix_cpu[i] += prefix_cpu[i - 1];
+    }
+
+
+    gpu::gpu_mem_32u cur_as, next_as, prefix_sum, cur_partial_sum, next_partial_sum;
+    cur_as.resizeN(n);
+    next_as.resizeN(n);
+    prefix_sum.resizeN(n);
+    cur_partial_sum.resizeN(n);
+    next_partial_sum.resizeN(n);
+
 
     {
+        ocl::Kernel get_inverse_bit(radix_kernel, radix_kernel_length, "get_inverse_bit");
+        get_inverse_bit.compile();
+        ocl::Kernel prefix_sum_kernel(radix_kernel, radix_kernel_length, "prefix_sum");
+        prefix_sum_kernel.compile();
+        ocl::Kernel partial_sum_kernel(radix_kernel, radix_kernel_length, "partial_sum");
+        partial_sum_kernel.compile();
         ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
         radix.compile();
 
         timer t;
+        //можно ли как то покрасивее это делать?
+        std::vector<unsigned int> zeros_array(n, 0);
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            as_gpu.writeN(as.data(), n);
-
+            cur_as.writeN(as.data(), n);
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
 
             unsigned int workGroupSize = 128;
             unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+            for(unsigned int bit = 0; bit <= std::log2(std::numeric_limits<int>::max())+1; ++bit) {
+                get_inverse_bit.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_as, cur_partial_sum, n, bit);
+                prefix_sum.writeN(zeros_array.data(), n);
+                for (unsigned int pow = 0; pow <= std::log2(n); ++pow) {
+                    prefix_sum_kernel.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_partial_sum, prefix_sum, n, pow);
+                    partial_sum_kernel.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_partial_sum, next_partial_sum, n);
+                    next_partial_sum.copyToN(cur_partial_sum, n);
+                }
+                radix.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_as, next_as, prefix_sum, n, bit);
+                next_as.copyToN(cur_as, n);
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
 
-        as_gpu.readN(as.data(), n);
+        cur_as.readN(as.data(), n);
     }
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
