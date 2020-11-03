ae478440d96a94a46257b4a037e84b0ae7cd8220
Тихон Воробьев <tihonvorobev@Ikhon.local>
2020-11-03 18:42:11 +0300

    update radix sort

--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -26,6 +26,7 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 int main(int argc, char **argv)
 {
+    argv[argc++] = "1";
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
     gpu::Context context;
@@ -33,7 +34,7 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 1024;
+    unsigned int n = 32 * 1024 * 1024;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -53,21 +54,11 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
 
-    std::vector<unsigned int> prefix_cpu = as;
-    for (unsigned int i = 1; i < n; ++i) {
-        prefix_cpu[i] += prefix_cpu[i - 1];
-    }
-
-
-    gpu::gpu_mem_32u cur_as, next_as, prefix_sum, cur_partial_sum, next_partial_sum;
-    cur_as.resizeN(n);
-    next_as.resizeN(n);
-    prefix_sum.resizeN(n);
-    cur_partial_sum.resizeN(n);
-    next_partial_sum.resizeN(n);
-
+    std::vector<unsigned int> gpu_sorted;
 
     {
+        gpu_sorted = as;
+
         ocl::Kernel get_inverse_bit(radix_kernel, radix_kernel_length, "get_inverse_bit");
         get_inverse_bit.compile();
         ocl::Kernel prefix_sum_kernel(radix_kernel, radix_kernel_length, "prefix_sum");
@@ -77,39 +68,112 @@ int main(int argc, char **argv)
         ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
         radix.compile();
 
+        unsigned int workGroupSize = 128;
+        unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
         timer t;
+
+        gpu::gpu_mem_32u cur_as, next_as, prefix_sum, initial_prefix_sum, cur_partial_sum, next_partial_sum;
+        cur_as.resizeN(n);
+        next_as.resizeN(n);
+        prefix_sum.resizeN(n);
+        cur_partial_sum.resizeN(n);
+        next_partial_sum.resizeN(n);
         //можно ли как то покрасивее это делать?
         std::vector<unsigned int> zeros_array(n, 0);
+        initial_prefix_sum.resizeN(n);
+
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             cur_as.writeN(as.data(), n);
+            initial_prefix_sum.writeN(zeros_array.data(), n);
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
-
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
             for(unsigned int bit = 0; bit <= std::log2(std::numeric_limits<int>::max())+1; ++bit) {
                 get_inverse_bit.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_as, cur_partial_sum, n, bit);
-                prefix_sum.writeN(zeros_array.data(), n);
+                initial_prefix_sum.copyToN(prefix_sum, n);
+                unsigned int cur_size = global_work_size;
                 for (unsigned int pow = 0; pow <= std::log2(n); ++pow) {
                     prefix_sum_kernel.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_partial_sum, prefix_sum, n, pow);
-                    partial_sum_kernel.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_partial_sum, next_partial_sum, n);
-                    next_partial_sum.copyToN(cur_partial_sum, n);
+                    cur_size /= 2;
+                    partial_sum_kernel.exec(gpu::WorkSize(workGroupSize, std::max(cur_size, workGroupSize)), cur_partial_sum, next_partial_sum, cur_size);
+                    next_partial_sum.copyToN(cur_partial_sum, cur_size);
                 }
                 radix.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_as, next_as, prefix_sum, n, bit);
+                t.nextLap();
                 next_as.copyToN(cur_as, n);
             }
-
-            t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
 
-        cur_as.readN(as.data(), n);
+        cur_as.readN(gpu_sorted.data(), n);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        EXPECT_THE_SAME(gpu_sorted[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+    }
+
+    /*
+
+    {
+        gpu_sorted = as;
+
+        ocl::Kernel tree_sum(radix_kernel, radix_kernel_length, "tree_sum");
+        tree_sum.compile();
+        ocl::Kernel prefix_sum_kernel(radix_kernel, radix_kernel_length, "prefix_sum");
+        prefix_sum_kernel.compile();
+        ocl::Kernel partial_sum_kernel(radix_kernel, radix_kernel_length, "partial_sum");
+        partial_sum_kernel.compile();
+        ocl::Kernel local_radix(radix_kernel, radix_kernel_length, "local_radix");
+        local_radix.compile();
+
+        unsigned int workGroupSize = 128;
+        unsigned int number_of_groups = (n + workGroupSize - 1) / workGroupSize;
+        unsigned int global_work_size =  number_of_groups * workGroupSize;
+
+        gpu::gpu_mem_32u cur_as, next_as, initial_prefix_sum, prefix_sum, cur_partial_sum, next_partial_sum;
+        cur_as.resizeN(n);
+        next_as.resizeN(n);
+
+        prefix_sum.resizeN(number_of_groups);
+        cur_partial_sum.resizeN(number_of_groups);
+        next_partial_sum.resizeN(number_of_groups);
+        std::vector<unsigned int> zeros_array(number_of_groups, 0);
+        initial_prefix_sum.resizeN(number_of_groups);
+
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            cur_as.writeN(as.data(), n);
+            //можно ли как то покрасивее это делать?
+            initial_prefix_sum.writeN(zeros_array.data(), number_of_groups);
+            t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
+            for(unsigned int bit = 0; bit <= std::log2(std::numeric_limits<int>::max())+1; ++bit) {
+                tree_sum.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_as, cur_partial_sum, n, bit);
+                //хотим вычислить значение префиксов перед work group
+                initial_prefix_sum.copyToN(prefix_sum, number_of_groups);
+                unsigned int cur_size = number_of_groups;
+                for (unsigned int pow = 0; pow <= std::log2(number_of_groups); ++pow) {
+                    prefix_sum_kernel.exec(gpu::WorkSize(workGroupSize, std::max(number_of_groups, workGroupSize)), cur_partial_sum, prefix_sum, number_of_groups, pow);
+                    cur_size /= 2;
+                    partial_sum_kernel.exec(gpu::WorkSize(workGroupSize, std::max(cur_size, workGroupSize)), cur_partial_sum, next_partial_sum, cur_size);
+                    next_partial_sum.copyToN(cur_partial_sum, cur_size);
+                }
+
+                local_radix.exec(gpu::WorkSize(workGroupSize, global_work_size), cur_as, next_as, prefix_sum, n, bit);
+                t.nextLap();
+                next_as.copyToN(cur_as, n);
+            }
+        }
+        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+
+        cur_as.readN(gpu_sorted.data(), n);
     }
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
-        EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+        EXPECT_THE_SAME(gpu_sorted[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
+     */
 
     return 0;
 }
